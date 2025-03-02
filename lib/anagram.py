d4a166993fd430019a4a05788bf88c4883ad7ba9
Arnold Mwangakala <arnoldmwangakala@gmail.com>
2025-03-02 17:51:41 +0300

    Complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, keyword):
+        self.keyword = keyword.lower()
+
+    def match(self, wordlist):
+            matchlist = []
+            for word in wordlist: 
+                if sorted(word.lower()) == sorted(self.keyword):
+                    matchlist.append(word)
+            return matchlist
+
+        
+
+Anagram("ByeGood").match(["Hello", "Goodbye", "Afternoon"])
